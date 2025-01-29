cff3e4c40dc347c5fc08d31968f57dc7f0d2cc34
saipulanwarr <saipulanwar180994@gmail.com>
2025-01-29 10:20:56 +0700

    create pages

---
+++ app/checkout/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CheckoutPage() {
+  return (
+    <h1 className='text-3xl'>CheckoutPage</h1>
+  )
+}
+
+export default CheckoutPage
---
+++ app/profile/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProfilePage() {
+  return (
+    <h1 className='text-3xl'>ProfilePage</h1>
+  )
+}
+
+export default ProfilePage
