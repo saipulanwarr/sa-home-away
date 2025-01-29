1fa0caa5f8ec28e3cf1a5e175f4bb72ece34a11c
saipulanwarr <saipulanwar180994@gmail.com>
2025-01-29 11:11:53 +0700

    shadcn/ui button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 function HomePage() {
   return (
-    <h1 className='text-3xl'>HomePage</h1>
+    <div>
+      <h1 className='text-3xl'>HomePage</h1>
+      <Button variant='outline' size='lg' className='capitalize m-8'>click me</Button>
+    </div>
   )
 }
 
