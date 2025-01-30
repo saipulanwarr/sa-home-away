c4f592997e751b8da1645e7cda05768c465ef5a3
saipulanwarr <saipulanwar180994@gmail.com>
2025-01-30 13:59:07 +0700

    links dropdown component

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,4 +1,4 @@
-"use-client";
+"use client";
 
 import React from "react";
 
@@ -7,6 +7,7 @@ import { useToast } from "@/hooks/use-toast";
 
 function SignOutLink() {
   const { toast } = useToast();
+
   const handleLogout = () => {
     toast({ description: "You have been signed out..." });
   };
