aeb0b9a3d75ff3cc41886b7c3eb128e7ce8e837f
saipulanwarr <saipulanwar180994@gmail.com>
2025-01-30 09:55:02 +0700

    setup dark mode with shadcn/ui

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import Logo from './Logo'
-import NavSearch from './NavSearch'
-import DarkMode from './DarkMode'
-import LinksDropdown from './LinksDropdown'
+import React from "react";
+import Logo from "./Logo";
+import NavSearch from "./NavSearch";
+import DarkMode from "./DarkMode";
+import LinksDropdown from "./LinksDropdown";
 
 function Navbar() {
   return (
-    <div className='border-b'>
-      <div className='container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8'>
+    <div className="border-b">
+      <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8">
         <Logo />
         <NavSearch />
-        <div className='flex gap-4 items-center'>
+        <div className="flex gap-4 items-center">
           <DarkMode />
           <LinksDropdown />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
