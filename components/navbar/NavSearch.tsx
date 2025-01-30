6dbc0e02ac610afa218ff6b71d856ea64fbd6c82
saipulanwarr <saipulanwar180994@gmail.com>
2025-01-30 09:09:16 +0700

    logo and navsearch component

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Input } from '../ui/input'
 
 function NavSearch() {
   return (
-    <div>NavSearch</div>
+    <Input type='text' placeholder='find a property...' className='max-w-xs dark:bg-muted' />
   )
 }
 
