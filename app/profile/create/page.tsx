81215b21295a380b0f69c3579e1d9c2a268e90f2
saipulanwarr <saipulanwar180994@gmail.com>
2025-01-31 10:40:22 +0700

    added zod library

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,15 +1,8 @@
 import { SubmitButton } from "@/components/form/Buttons";
 import FormContainer from "@/components/form/FormContainer";
 import FormInput from "@/components/form/FormInput";
+import { createProfileAction } from "@/utils/actions";
 import React from "react";
-
-const createProfileAction = async (prevState: any, formData: FormData) => {
-  "use server";
-  const firstName = formData.get("firstName");
-  console.log("firstName", firstName);
-
-  return { message: "Profile created" };
-};
 
 function CreateProfilePage() {
   return (
