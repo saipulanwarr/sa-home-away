f2cd2d2d3298ba85fecd147a1f5cc8eda69746b0
saipulanwarr <saipulanwar180994@gmail.com>
2025-02-02 09:43:16 +0700

    fetch profile user

--- utils/actions.ts
+++ utils/actions.ts
@@ -6,6 +6,16 @@ import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+const getAuthUser = async () => {
+    const user = await currentUser()
+    if(!user){
+        throw new Error('You must be logged in access this route');
+    }
+    if(!user.privateMetadata.hasProfile) redirect('/profile/create')
+
+    return user;
+}
+
 export const createProfileAction = async (prevState: any, formData: FormData) => {
     try {
         const user = await currentUser();
@@ -49,5 +59,21 @@ export const createProfileAction = async (prevState: any, formData: FormData) =>
     });
 
     return profile?.profileImage;
+  }
+
+  export const fetchProfile = async () => {
+    const user = await getAuthUser();
+    const profile = await db.profile.findUnique({
+        where: {
+            clerkId: user.id,
+        },
+    });
+    if(!profile) redirect('/profile/create')
+
+    return profile
+  }
+
+  export const updateProfileAction = async (prevState: any, formData: FormData): Promise<{message: string}> => {
+    return {message : 'update profile action'}
   }
   
