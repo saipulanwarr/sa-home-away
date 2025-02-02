f20080b122d58506dc02c5622d76b33dcaafc1b0
saipulanwarr <saipulanwar180994@gmail.com>
2025-02-02 10:32:51 +0700

    added image input component

---
+++ components/form/ImageInput.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+
+function ImageInput() {
+  const name = "image";
+
+  return (
+    <div className="mb-2">
+      <Label htmlFor={name} className="capitalize">
+        Image
+      </Label>
+      <Input
+        id={name}
+        name={name}
+        type="file"
+        accept="image/*"
+        className="max-w-ws"
+      />
+    </div>
+  );
+}
+
+export default ImageInput;
