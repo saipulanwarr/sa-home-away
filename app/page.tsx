b3a4010dcb786936230cb48b4f804ad750ded590
saipulanwarr <saipulanwar180994@gmail.com>
2025-02-04 09:23:37 +0700

    added home page setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
 
-function HomePage() {
+import CategoriesList from "@/components/home/CategoriesList";
+import PropertiesContainer from "@/components/home/PropertiesContainer";
+
+function HomePage({
+  searchParams,
+}: {
+  searchParams: { category?: string; search?: string };
+}) {
+  console.log("searchParams", searchParams);
   return (
-    <div>
-      <h1 className="text-3xl">HomePage</h1>
-      <Button variant="outline" size="lg" className="capitalize m-8">
-        click me
-      </Button>
-    </div>
+    <section>
+      <CategoriesList
+        category={searchParams.category}
+        search={searchParams.search}
+      />
+      <PropertiesContainer
+        category={searchParams.category}
+        search={searchParams.search}
+      />
+    </section>
   );
 }
 
