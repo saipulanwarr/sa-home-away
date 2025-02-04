6d2615cd8bfdfc73949ee1350466e0008a9a3b07
saipulanwarr <saipulanwar180994@gmail.com>
2025-02-04 15:47:25 +0700

    added toggle favorites functionality

--- components/card/FavoriteToggleForm.tsx
+++ components/card/FavoriteToggleForm.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
 
-function FavoriteToggleForm() {
-  return <div>FavoriteToggleForm</div>;
+import React from "react";
+import { toggleFavoriteAction } from "@/utils/actions";
+import { usePathname } from "next/navigation";
+import FormContainer from "../form/FormContainer";
+import { CardSubmitButton } from "../form/Buttons";
+
+type FavoriteToggleFormProps = {
+  propertyId: string;
+  favoriteId: string | null;
+};
+
+function FavoriteToggleForm({
+  propertyId,
+  favoriteId,
+}: FavoriteToggleFormProps) {
+  const pathname = usePathname();
+  const toggleAction = toggleFavoriteAction.bind(null, {
+    propertyId,
+    favoriteId,
+    pathname,
+  });
+  return (
+    <FormContainer action={toggleAction}>
+      <CardSubmitButton isFavorite={favoriteId ? true : false} />
+    </FormContainer>
+  );
 }
 
 export default FavoriteToggleForm;
