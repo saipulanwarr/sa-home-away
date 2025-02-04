554ac12b7f7e8ae5d0fa1cb4e35d4aa85ee46ff5
saipulanwarr <saipulanwar180994@gmail.com>
2025-02-04 14:58:27 +0700

    added favorites signin button

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Button } from "../ui/button";
 import { FaHeart } from "react-icons/fa";
+import { auth } from "@clerk/nextjs/server";
+import { CardSignInButton } from "../form/Buttons";
 
 function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
+  const { userId } = auth();
+  if (!userId) return <CardSignInButton />;
   return (
     <Button size="icon" variant="outline" className="p-2 cursor-pointer">
       <FaHeart />
--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -5,6 +5,8 @@ import React from "react";
 import { ReloadIcon } from "@radix-ui/react-icons";
 import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
 
 type btnSize = "default" | "lg" | "sm";
 
@@ -39,3 +41,19 @@ export function SubmitButton({
     </Button>
   );
 }
+
+export const CardSignInButton = () => {
+  return (
+    <SignInButton mode="modal">
+      <Button
+        type="button"
+        size="icon"
+        variant="outline"
+        className="p-2 cursor-pointer"
+        asChild
+      >
+        <FaRegHeart />
+      </Button>
+    </SignInButton>
+  );
+};
