892b0b64f26941c05037a9c6cd35e9408067da48
saipulanwarr <saipulanwar180994@gmail.com>
2025-02-05 10:25:43 +0700

    added property details component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -7,13 +7,14 @@ import ShareButton from "@/components/properties/ShareButton";
 import ImageContainer from "@/components/properties/ImageContainer";
 import PropertyRating from "@/components/card/PropertyRating";
 import BookingCalendar from "@/components/properties/BookingCalendar";
+import PropertyDetails from "@/components/properties/PropertyDetails";
 
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const property = await fetchPropertyDetails(params.id);
   if (!property) redirect("/");
 
   const { baths, bedrooms, beds, guests } = property;
-  const detail = { baths, bedrooms, beds, guests };
+  const details = { baths, bedrooms, beds, guests };
 
   return (
     <section>
@@ -32,6 +33,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
             <h1 className="text-xl font-bold">{property.name}</h1>
             <PropertyRating inPage propertyId={property.id} />
           </div>
+          <PropertyDetails details={details} />
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
           <BookingCalendar />
