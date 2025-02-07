5f28d4fb382e6039e9a0586ed90660c138be4641
saipulanwarr <saipulanwar180994@gmail.com>
2025-02-07 10:44:57 +0700

    added booking container component

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+"use client";
+import { defaultSelected } from "@/utils/calendar";
+import React, { useEffect, useState } from "react";
+import { DateRange } from "react-day-picker";
+import { Calendar } from "../ui/calendar";
+import { useProperty } from "@/utils/store";
 
 function BookingCalendar() {
-  return <div>BookingCalendar</div>;
+  const currentDate = new Date();
+  const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
+
+  useEffect(() => {
+    useProperty.setState({
+      range,
+    });
+  }, [range]);
+
+  return (
+    <Calendar
+      mode="range"
+      defaultMonth={currentDate}
+      selected={range}
+      onSelect={setRange}
+      className="mb-4"
+    />
+  );
 }
 
 export default BookingCalendar;
--- components/booking/BookingContainer.tsx
+++ components/booking/BookingContainer.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { useProperty } from "@/utils/store";
 import React from "react";
+import BookingForm from "./BookingForm";
+import ConfirmBooking from "./ConfirmBooking";
 
 function BookingContainer() {
-  return <div>BookingContainer</div>;
+  const state = useProperty((state) => state);
+  console.log("state", state);
+
+  return (
+    <div className="w-full">
+      <BookingForm />
+      <ConfirmBooking />
+    </div>
+  );
 }
 
 export default BookingContainer;
