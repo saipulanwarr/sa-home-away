1ad0993c80bd0e68e87906977e0d802d8ad104c2
saipulanwarr <saipulanwar180994@gmail.com>
2025-02-08 11:25:15 +0700

    added block period

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -1,15 +1,44 @@
 "use client";
-import { defaultSelected } from "@/utils/calendar";
+import {
+  defaultSelected,
+  generateBlockedPeriods,
+  generateDateRange,
+  generateDisabledDates,
+} from "@/utils/calendar";
 import React, { useEffect, useState } from "react";
 import { DateRange } from "react-day-picker";
 import { Calendar } from "../ui/calendar";
 import { useProperty } from "@/utils/store";
+import { useToast } from "@/hooks/use-toast";
 
 function BookingCalendar() {
   const currentDate = new Date();
   const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
+  const bookings = useProperty((state) => state.bookings);
+  const { toast } = useToast();
+
+  const blockedPeriods = generateBlockedPeriods({
+    bookings,
+    today: currentDate,
+  });
+
+  const unavaliableDates = generateDisabledDates(blockedPeriods);
 
   useEffect(() => {
+    const selectedRange = generateDateRange(range);
+    const isDisabledDateIncluded = selectedRange.some((date) => {
+      if (unavaliableDates[date]) {
+        setRange(defaultSelected);
+        toast({
+          description: "Some date are blocked. Please select again",
+        });
+        return true;
+      }
+      return false;
+    });
+
+    console.log(isDisabledDateIncluded);
+
     useProperty.setState({
       range,
     });
@@ -22,6 +51,7 @@ function BookingCalendar() {
       selected={range}
       onSelect={setRange}
       className="mb-4"
+      disabled={blockedPeriods}
     />
   );
 }
